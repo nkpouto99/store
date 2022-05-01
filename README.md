80e37771decf190d31a2937b51fa42c4746d5cba
nkpouto99 <99331869+nkpouto99@users.noreply.github.com>
2022-05-02 00:42:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Francis-Pet_
---
+++ script.js
@@ -0,0 +1,5 @@
+const header = document.getElementsByClassName = ("header")
+
+header.addEventListener('hover',  function(e){
+    header.style.BackgroundColor="black"
+})
